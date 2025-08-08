0340c01ab12efb11b0d70d91ce44eaf7aa12047d
domt <124377454+dominictory@users.noreply.github.com>
2025-08-08 11:26:43 +0100

    Update sysmon-nazarx.ps1

--- sysmon-nazarx.ps1
+++ sysmon-nazarx.ps1
@@ -10,6 +10,9 @@ function Log-Message {
     Write-Output "[$severity] $message"
 }
 
+# Initialize success/error code
+$successCode = 1
+
 try {
     # Log start of script
     Log-Message "Starting script execution."
@@ -22,14 +25,32 @@ try {
     Log-Message "Waiting for installation to complete..."
     Start-Sleep -s 30
     Log-Message "Starting Wazuh service..."
-    NET START WazuhSvc
+
+    # Try to start the service and handle errors
+    try {
+        NET START WazuhSvc
+    } catch {
+        Log-Message "Failed to start Wazuh service: $_" -severity "ERROR"
+        $successCode = 0
+        throw
+    }
+
     Log-Message "Wazuh agent installation completed successfully."
 
     # Download and run Sysmon installation script
     Log-Message "Downloading Sysmon installation script..."
     Invoke-WebRequest -Uri https://raw.githubusercontent.com/Communicate-Technology/sysmon-modular/master/sysmon_install.ps1 -OutFile "$tempDir\sysmon_install.ps1"
     Log-Message "Running Sysmon installation script..."
-    powershell -ep Bypass "$tempDir\sysmon_install.ps1"
+
+    # Try to run the Sysmon script and handle errors
+    try {
+        powershell -ep Bypass "$tempDir\sysmon_install.ps1"
+    } catch {
+        Log-Message "Failed to run Sysmon installation script: $_" -severity "ERROR"
+        $successCode = 0
+        throw
+    }
+
     Log-Message "Sysmon installation completed successfully."
 
     # Log successful completion
@@ -37,5 +58,9 @@ try {
 } catch {
     # Log error
     Log-Message "An error occurred: $_" -severity "ERROR"
+    $successCode = 0
     throw
+} finally {
+    # Output success/error code
+    Write-Output "Exit code: $successCode"
 }
