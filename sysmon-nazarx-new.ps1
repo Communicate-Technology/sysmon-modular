7bea1a12cc7dd789ac8e5788ce4d800cee8a53b4
domt <124377454+dominictory@users.noreply.github.com>
2025-08-12 14:10:56 +0100

    Update sysmon-nazarx-new.ps1

--- sysmon-nazarx-new.ps1
+++ sysmon-nazarx-new.ps1
@@ -2,25 +2,31 @@ $tempDir = "$env:TEMP"
 
 function Log-Message { param ( [string]$message, [string]$severity = "INFO" ) Write-Output "[$severity] $message" }
 
-$successCode = 0  # Initialize as failure
+$successCode = 0
 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 
 try { Log-Message "Starting script execution."
 
-    Log-Message "Downloading Wazuh agent..."
-    Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.10.1-1.msi -OutFile "$tempDir\wazuh-agent.msi"
-    Log-Message "Installing Wazuh agent..."
-    msiexec.exe /i "$tempDir\wazuh-agent.msi" /q WAZUH_MANAGER='192.9.200.190' WAZUH_REGISTRATION_SERVER='192.9.200.190'
-    Log-Message "Waiting for installation to complete..."
-    Start-Sleep -s 30
-    Log-Message "Starting Wazuh service..."
+    Log-Message "Checking if Wazuh service is already running..."
+    $wazuhService = Get-Service -Name "WazuhSvc" -ErrorAction SilentlyContinue
+    if ($wazuhService -and $wazuhService.Status -eq 'Running') {
+        Log-Message "Wazuh service is already running. Skipping installation."
+    } else {
+        Log-Message "Downloading Wazuh agent..."
+        Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.10.1-1.msi -OutFile "$tempDir\wazuh-agent.msi"
+        Log-Message "Installing Wazuh agent..."
+        msiexec.exe /i "$tempDir\wazuh-agent.msi" /q WAZUH_MANAGER='192.9.200.190' WAZUH_REGISTRATION_SERVER='192.9.200.190'
+        Log-Message "Waiting for installation to complete..."
+        Start-Sleep -s 30
+        Log-Message "Starting Wazuh service..."
 
-    try {
-        NET START WazuhSvc
-    } catch {
-        Log-Message "Failed to start Wazuh service: $_" -severity "ERROR"
-        throw
+        try {
+            NET START WazuhSvc
+        } catch {
+            Log-Message "Failed to start Wazuh service: $_" -severity "ERROR"
+            throw
+        }
     }
 
     Log-Message "Wazuh agent installation completed successfully."
@@ -50,5 +56,5 @@ try { Log-Message "Starting script execution."
     Log-Message "Sysmon installation completed successfully."
 
     Log-Message "Script execution completed successfully." -severity "SUCCESS"
-    $successCode = 1
+    $successCode = 0
 } catch { Log-Message "An error occurred: $_" -severity "ERROR" throw } finally { Write-Output "Exit code: $successCode" }
